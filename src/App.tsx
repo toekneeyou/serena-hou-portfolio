f975177a9f6376b4eb958e9566379dfeb87422e1
Tony <tonyyu1129@gmail.com>
2024-08-18 11:33:39 -0700

    added outlet and viewport oberver to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/header/Header";
+import ViewportObserver from "./components/viewportObserver/ViewportObserver";
 import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <h1 style={{ fontWeight: "bold" }}>MULTI-DISCPLINARLY</h1>
+      <ViewportObserver />
+      <Header />
+      <Outlet />
     </>
   );
 }
