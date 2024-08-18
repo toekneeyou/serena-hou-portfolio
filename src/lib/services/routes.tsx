af1377062a1d86a9d73edc581eedde2e509c5ad5
Tony <tonyyu1129@gmail.com>
2024-08-17 22:17:08 -0700

    setup routes

---
+++ src/lib/services/routes.tsx
@@ -0,0 +1,102 @@
+import { createBrowserRouter, RouteObject } from "react-router-dom";
+import App from "../../App";
+
+class AppRoute {
+  name: string;
+  id: string;
+  path: RouteObject["path"];
+  element?: RouteObject["element"];
+  lazy?: RouteObject["lazy"];
+  children?: AppRoute[];
+
+  constructor({ name, id, path, element, lazy, children }: AppRoute) {
+    this.name = name;
+    this.id = id;
+    this.path = path;
+    this.element = element;
+    this.lazy = lazy;
+    this.children = children ?? [];
+  }
+}
+
+export const homeRoute = new AppRoute({
+  name: "Home",
+  id: "home",
+  path: "/",
+  element: <App />,
+});
+const projectRoute = new AppRoute({
+  name: "Project",
+  id: "project",
+  path: "/#project",
+  lazy: async () => {
+    const { ProjectView } = await import("../../views/project/ProjectView");
+    return { Component: ProjectView };
+  },
+});
+const visualRoute = new AppRoute({
+  name: "Visual",
+  id: "visual",
+  path: "visual",
+  lazy: async () => {
+    const { VisualView } = await import("../../views/visual/VisualVisual");
+    return { Component: VisualView };
+  },
+});
+const videoRoute = new AppRoute({
+  name: "Video",
+  id: "video",
+  path: "video",
+  lazy: async () => {
+    const { VideoView } = await import("../../views/video/VideoView");
+    return { Component: VideoView };
+  },
+});
+const aboutRoute = new AppRoute({
+  name: "About",
+  id: "about",
+  path: "about",
+  lazy: async () => {
+    const { AboutView } = await import("../../views/about/AboutView");
+    return { Component: AboutView };
+  },
+});
+const contactRoute = new AppRoute({
+  name: "Contact",
+  id: "contact",
+  path: "contact",
+  lazy: async () => {
+    const { ContactView } = await import("../../views/contact/ContactView");
+    return { Component: ContactView };
+  },
+});
+
+homeRoute.children?.push(
+  projectRoute,
+  visualRoute,
+  videoRoute,
+  aboutRoute,
+  contactRoute
+);
+
+const convertAppRouteToRouterRoute = (appRoutes: AppRoute[]) => {
+  return appRoutes.map((ar) => {
+    const routeObject: RouteObject = {
+      path: ar.path,
+    };
+    if (ar.element) {
+      routeObject.element = ar.element;
+    }
+    if (ar.lazy) {
+      routeObject.lazy = ar.lazy;
+    }
+    if (ar.children && ar.children.length > 0) {
+      routeObject.children = convertAppRouteToRouterRoute(ar.children);
+    }
+    return routeObject;
+  });
+};
+
+const routerRoutes = convertAppRouteToRouterRoute([homeRoute]);
+
+export const router = createBrowserRouter(routerRoutes);
