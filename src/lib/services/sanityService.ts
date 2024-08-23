4f7281121825d3c263ae2d7fedad689cb93b1184
Tony <tonyyu1129@gmail.com>
2024-08-22 22:12:00 -0700

    install sanity client

---
+++ src/lib/services/sanityService.ts
@@ -0,0 +1,47 @@
+import { createClient } from "@sanity/client";
+
+export const sanityClient = createClient({
+  projectId: "iaut86sl",
+  dataset: "production",
+  useCdn: true,
+  apiVersion: "2023-05-03",
+});
+
+const handleNoResults = (response: unknown) => {
+  if (Array.isArray(response) && response.length === 0) {
+    throw new Error("No results");
+  }
+};
+
+export const fetchVideosAndCategories = async () => {
+  const response = await sanityClient.fetch(`*[_type == "videoCategory"]{
+    _id,
+    name,
+    title,
+    video[]->{
+      _id,
+      name,
+      "thumbnailUrl": thumbnail.asset->url,
+      category->{
+        _id,
+      }
+    },
+  }
+`);
+  handleNoResults(response);
+  return response;
+};
+
+export const fetchVisuals = async () => {
+  const response = await sanityClient.fetch(`*[_type == "visual"]{
+    _id,
+    name,
+    title,
+    date,
+    tags,
+    "image": image.asset->url
+    }
+  `);
+  handleNoResults(response);
+  return response;
+};
