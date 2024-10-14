dfd7fc99f7ea1bed3efdd6bb82e726b14b52b52f
Tony <tonyyu1129@gmail.com>
2024-10-13 17:07:33 -0700

    GhostWriterSideNav

---
+++ src/views/caseStudy/caseStudies/ghostWriter/ghostWriterConstants.ts
@@ -0,0 +1,25 @@
+export const GW_INTRODUCTION_ID = "gw-introduction";
+export const GW_PROBLEM_ID = "gw-problem";
+export const GW_GOAL_ID = "gw-goal";
+export const GW_APPROACH_ID = "gw-approach";
+export const GW_FEEDBACK_ID = "gw-feedback";
+export const GW_ISSUES_ID = "gw-issues";
+export const GW_RESEARCH_ID = "gw-research";
+export const GW_REDESIGN_ID = "gw-redesign";
+export const GW_USER_INSIGHTS_ID = "gw-user-insights";
+export const GW_RESULTS_ID = "gw-results";
+export const GW_TAKEAWAY_ID = "gw-takeaway";
+
+export const GW_SECTIONS = [
+  { display: "Introduction", id: GW_INTRODUCTION_ID },
+  { display: "The Problem", id: GW_PROBLEM_ID },
+  { display: "The Goal", id: GW_GOAL_ID },
+  { display: "My Approach", id: GW_APPROACH_ID },
+  { display: "The Feedback", id: GW_FEEDBACK_ID },
+  { display: "The Issues", id: GW_ISSUES_ID },
+  { display: "Research", id: GW_RESEARCH_ID },
+  { display: "Redesign", id: GW_REDESIGN_ID },
+  { display: "User Insights", id: GW_USER_INSIGHTS_ID },
+  { display: "Results", id: GW_RESULTS_ID },
+  { display: "Key Takeaway", id: GW_TAKEAWAY_ID },
+];
