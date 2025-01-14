bd7952e53966f2ef3f5d9eaa7c83e52c01ca31a3
Tony <tonyyu1129@gmail.com>
2025-01-13 21:24:04 -0800

    add appropriate height to TitleRoll

--- src/views/video/components/TitleRoll.tsx
+++ src/views/video/components/TitleRoll.tsx
@@ -1,7 +1,9 @@
 import Roll from "@components/roll/Roll";
-import { FC } from "react";
+import { FC, useMemo } from "react";
 import { VideoCategory } from "../types";
 import { useSearchParams } from "react-router-dom";
+import { useViewportState } from "@contexts/viewport/hooks";
+import { pxToRem } from "@helpers/conversions";
 
 interface TitleRollProps {
   sortedVideos: VideoCategory[];
@@ -9,13 +11,27 @@ interface TitleRollProps {
 
 export const TitleRoll: FC<TitleRollProps> = ({ sortedVideos }) => {
   const [params] = useSearchParams();
+  const { viewportSize } = useViewportState();
   const itemRenderFunction = (video: VideoCategory) => {
     return <h2 className={"title"}>{video.title}</h2>;
   };
 
+  const height = useMemo(() => {
+    switch (viewportSize) {
+      case "3xl":
+      case "2xl":
+      case "xl":
+      case "lg":
+      case "md":
+        return pxToRem(120);
+      default:
+        return pxToRem(40);
+    }
+  }, [viewportSize]);
+
   return (
     <Roll
-      height="40px"
+      height={height}
       alignment="center"
       currIndex={sortedVideos.findIndex(
         (video) => video.name === params.get("category")
